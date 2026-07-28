460f1be4aa85845e723c2c569b90f988f1e324ce
Yusom-Tech <yunusomomoh@yahoo.com>
2026-07-28 11:39:38 +0100

    ApiInstall

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,17 +4,11 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\Auth\AuthController;
 
 Route::prefix('auth')->group(function () {
-
     Route::post('/register', [AuthController::class, 'register']);
-
     Route::post('/login', [AuthController::class, 'login']);
 
     Route::middleware('auth:sanctum')->group(function () {
-
         Route::post('/logout', [AuthController::class, 'logout']);
-
         Route::get('/profile', [AuthController::class, 'profile']);
-
     });
-
 });
